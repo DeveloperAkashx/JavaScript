396c1fb330212b3cae96091d2c9d992bb97d9779
Akash Patil <159530461+DeveloperAkashx@users.noreply.github.com>
2025-07-10 08:24:21 +0530

    Functions

---
+++ Functions.js
@@ -0,0 +1,27 @@
+// function simple() {
+//     console.log("Hello world");
+// }
+
+// simple();
+
+
+
+
+// performing a task
+function simple(name, lastName) {
+    console.log("Hello " + name +" "+ lastName);
+}
+
+simple('Akash', 'Patil');
+
+
+
+
+// Calculating a value
+function cal(num1,num2){
+    console.log(num1+num2)
+}
+
+cal(5,5);
+cal(425,632);
+
