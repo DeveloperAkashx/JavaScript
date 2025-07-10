0ccdf61676a6e9099d630370d271d0df7d7fb3bf
Akash Patil <159530461+DeveloperAkashx@users.noreply.github.com>
2025-07-10 08:21:42 +0530

    JavaScript Starts

---
+++ Script.js
@@ -0,0 +1,35 @@
+let name = 'Akash';
+
+console.log(name);
+
+let largeNum = 999;
+console.log(largeNum)
+
+
+// let interestRate = 3;
+// interestRate = 8;
+// console.log(interestRate);
+
+// Constant
+
+// const interestRate = 3;
+// interestRate = 8;
+// console.log(interestRate);
+
+// let name2 = "Patil";
+// let age = 20;
+// let isAproval = false;
+// let firstName = undefined;
+// let lastName = null;
+// console.log(name+name2);
+
+// let person = {
+//     naam: 'viraj',
+//     vay: 18
+// };
+
+// person.naam = 'Shaka'   // Dot notation
+
+// person['naam'] = 'xyz'  // Bracket notation
+
+// console.log(person)
